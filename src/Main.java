2782d2fc5d1a2158191d28dab513001f358eacf0
Evgen <evgen.tm959@gmail.com>
2024-12-19 15:46:45 +0100

    task03-no-controller

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,51 @@
 
 /*
-3. Find the sum of the digits in the number 100! (i.e. 100 factorial)
-{Correct answer: 648}
+2. You are given a list of cities. Each direct connection between two cities has its transportation cost (an integer bigger than 0). The goal is to find the paths of minimum cost between pairs of cities. Assume that the cost of each path (which is the sum of costs of all direct connections belonging to this path) is at most 200000. The name of a city is a string containing characters a,...,z and is at most 10 characters long.2)
 
+Input:
+s [the number of tests <= 10]
+n [the number of cities <= 10000]
+NAME [city name]
+p [the number of neighbors of city NAME]
+nr cost [nr - index of a city connected to NAME (the index of the first city is 1)]
+           [cost - the transportation cost]
+r [the number of paths to find <= 100]
+NAME1 NAME2 [NAME1 - source, NAME2 - destination]
+[empty line separating the tests]
+
+Output:
+cost [the minimum transportation cost from city NAME1 to city NAME2 (one per line)]
+
+Example:
+
+Input:
+1
+4
+gdansk
+2
+2 1
+3 3
+bydgoszcz
+3
+1 1
+3 1
+4 4
+torun
+3
+1 3
+2 1
+4 1
+warszawa
+2
+2 4
+3 1
+2
+gdansk warszawa
+bydgoszcz warszawa
+
+Output:
+3
+2
  */
 
 
@@ -10,41 +53,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Vertex A = new Vertex("A");
-        Vertex B = new Vertex("B");
-        Vertex C = new Vertex("C");
-        Vertex D = new Vertex("D");
-        Vertex E = new Vertex("E");
-        Vertex F = new Vertex("F");
-        Vertex G = new Vertex("G");
-        Vertex H = new Vertex("H");
+        Vertex gdansk = new Vertex("gdansk");
+        Vertex bydgoszcz = new Vertex("bydgoszcz");
+        Vertex torun = new Vertex("torun");
+        Vertex warszawa = new Vertex("warszawa");
 
-        A.addNeighbour(new Edge(5, A, B));
-        A.addNeighbour(new Edge(8, A, H));
-        A.addNeighbour(new Edge(9, A, E));
-        B.addNeighbour(new Edge(4, B, H));
-        B.addNeighbour(new Edge(12, B, C));
-        B.addNeighbour(new Edge(15, B, D));
-        C.addNeighbour(new Edge(3, C, D));
-        C.addNeighbour(new Edge(11, C, G));
-        D.addNeighbour(new Edge(9, D, G));
-        E.addNeighbour(new Edge(5, E, H));
-        E.addNeighbour(new Edge(4, E, F));
-        E.addNeighbour(new Edge(20, E, G));
-        F.addNeighbour(new Edge(1, F, C));
-        F.addNeighbour(new Edge(13, F, G));
-        H.addNeighbour(new Edge(7, H, C));
-        H.addNeighbour(new Edge(6, H, F));
+        gdansk.addNeighbour(new Edge(1, gdansk, bydgoszcz));
+        gdansk.addNeighbour(new Edge(3, gdansk, torun));
+        bydgoszcz.addNeighbour(new Edge(1, bydgoszcz, gdansk));
+        bydgoszcz.addNeighbour(new Edge(1, bydgoszcz, torun));
+        bydgoszcz.addNeighbour(new Edge(4, bydgoszcz, warszawa));
+        torun.addNeighbour(new Edge(3, torun, gdansk));
+        torun.addNeighbour(new Edge(1, torun, bydgoszcz));
+        torun.addNeighbour(new Edge(1, torun, warszawa));
+        warszawa.addNeighbour(new Edge(4, warszawa, bydgoszcz));
+        warszawa.addNeighbour(new Edge(1, warszawa, torun));
+
 
         Dijkstra dijkstra = new Dijkstra();
-        // запускаем програму и указываем что вершину с которой начитаем расчет
-        // в данном случае вершина "А"
-        dijkstra.compute(A);
-        // указываем вершину "G" до каторой надо расчитать кратчайший путь
-        System.out.println(G.getDistance());
+        // запускаем программу и указываем что вершину с которой начитаем расчет
+        // в данном случае вершина "gdansk"
+        dijkstra.compute(gdansk);
+        // указываем вершину "warszawa" до каторой надо расчитать кратчайший путь
+        System.out.println(warszawa.getDistance());
 
         // запускаем метод который выводит на экран все вершины через которые проходит наш путь
-        dijkstra.showPath(G);
+        dijkstra.showPath(warszawa);
 
 
 
